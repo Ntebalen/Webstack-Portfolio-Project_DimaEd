85fe31ca30ea6000a434bdde8d20dd8a1b20f8ea
Ntebalen <100698269+Ntebalen@users.noreply.github.com>
2023-12-14 18:38:54 +0200

    Add files via upload

---
+++ src/components/Header/Card/Card.jsx
@@ -0,0 +1,13 @@
+import { CardContainer, TitleContainer, TextContainer } from "./Card.styles";
+const Card = () => {
+  return (
+    <CardContainer>
+      <TitleContainer>Explore Your Ambitions!</TitleContainer>
+      <TextContainer>
+       Dive into the learning adventure and acquire the skills to turn your dreams into reality.
+      </TextContainer>
+    </CardContainer>
+  );
+};
+
+export default Card;
